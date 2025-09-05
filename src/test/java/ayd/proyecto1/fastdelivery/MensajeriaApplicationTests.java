f7ac6cb73ed5bf25985455e1f7ef88216d0c5d1b
DiegoAvilaEstrada <de@t2mc.net>
2025-09-04 22:49:10 -0600

    init project

---
+++ src/test/java/ayd/proyecto1/fastdelivery/MensajeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package ayd.proyecto1.fastdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MensajeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
