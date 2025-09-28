7103246c1591e106ea6fa2b8dd9b864912b6f750
DiegoAvilaEstrada <de@t2mc.net>
2025-09-28 11:51:24 -0600

    add receipt entity

---
+++ src/main/java/ayd/proyecto1/fastdelivery/repository/entities/Receipt.java
@@ -0,0 +1,25 @@
+package ayd.proyecto1.fastdelivery.repository.entities;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "receipt")
+public class Receipt {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_delivery_order_assignment", referencedColumnName = "id")
+    private DeliveryOrderAssignment deliveryOrderAssignment;
+
+    private Double amount;
+
+    private Date date;
+
+}
